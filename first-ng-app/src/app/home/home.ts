6085be103e875bb7402b5eeb98c1abbf2a8f971a
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-07-14 15:09:04 +0200

    Angular Services

--- first-ng-app/src/app/home/home.ts
+++ first-ng-app/src/app/home/home.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
 import { Counter } from '../components/counter/counter';
+// import { TodosService } from '../services/todos';
 
 @Component({
   selector: 'app-home',
   imports: [Greeting, Counter],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
+  // to make the service available ONLY to the home page + delete whatever (providedIn) is inside de injectable declaration in todos.ts
+  // providers:[
+  //   Todos
+  // ]
 })
 export class Home {
   homeMessage = signal("Hello World!");
