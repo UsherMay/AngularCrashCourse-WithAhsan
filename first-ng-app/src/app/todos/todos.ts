b4400a6633562b9c1ddff47d6e382554a8588a27
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-07-14 16:31:09 +0200

    Angular Pipes
    used to transform data right in the templates

--- first-ng-app/src/app/todos/todos.ts
+++ first-ng-app/src/app/todos/todos.ts
@@ -3,19 +3,20 @@ import { TodosService } from '../services/todos'
 import { Todo } from '../model/todo.types';
 import { catchError } from 'rxjs';
 import { TodoItem } from "../components/todo-item/todo-item";
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos-pipe';
 // import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [
-    TodoItem
-],
+  imports: [ TodoItem, FormsModule, FilterTodosPipe],
   templateUrl: './todos.html',
   styleUrl: './todos.scss'
 })
 export class Todos implements OnInit{
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal<string>('');
 
   ngOnInit(): void {
     // console.log(this.todoService.todoItems);
