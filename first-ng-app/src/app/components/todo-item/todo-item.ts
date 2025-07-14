f4c6c6d66ca051c3f6cfb4b5a35a8a92efd8e09a
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-07-14 16:16:15 +0200

    Angular Directives

--- first-ng-app/src/app/components/todo-item/todo-item.ts
+++ first-ng-app/src/app/components/todo-item/todo-item.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Todo } from '../../model/todo.types';
+import { HighlightCompletedTodo } from '../../directives/highlight-completed-todo';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [HighlightCompletedTodo],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss'
 })
 export class TodoItem {
+  todo = input.required<Todo>();
 
+  todoToggle = output<Todo>();
+
+  todoClicked(){
+    this.todoToggle.emit(this.todo());
+  }
 }
--- first-ng-app/src/app/todos/todos.ts
+++ first-ng-app/src/app/todos/todos.ts
@@ -2,10 +2,14 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos'
 import { Todo } from '../model/todo.types';
 import { catchError } from 'rxjs';
+import { TodoItem } from "../components/todo-item/todo-item";
+// import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [
+    TodoItem
+],
   templateUrl: './todos.html',
   styleUrl: './todos.scss'
 })
@@ -24,5 +28,19 @@ export class Todos implements OnInit{
       ).subscribe((todos)=>{
         this.todoItems.set(todos);
       }) ;
+  }
+
+  updateTodoItem(todoItem: Todo){
+    this.todoItems.update( (todos) => {
+      return todos.map( (todo)=>{
+        if(todo.id == todoItem.id){
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+    });
   }
 }
